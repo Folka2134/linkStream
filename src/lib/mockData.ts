6b9d883e3df44b152e497e12c8ab7606cbc9ca6a
folka2134 <folka2134@gmail.com>
2024-08-27 11:48:33 +0100

    created mockdata

---
+++ src/lib/mockData.ts
@@ -0,0 +1,32 @@
+export const groups = [
+  {
+    id: 4,
+    name: "Direct Messages",
+    image: "https://via.placeholder.com/50",
+    muted: false,
+  },
+  {
+    id: 1,
+    name: "Social",
+    image: "https://via.placeholder.com/50",
+    muted: false,
+  },
+  {
+    id: 2,
+    name: "News",
+    image: "https://via.placeholder.com/50",
+    muted: false,
+  },
+  {
+    id: 3,
+    name: "Gamers",
+    image: "https://via.placeholder.com/50",
+    muted: false,
+  },
+  {
+    id: 4,
+    name: "Development",
+    image: "https://via.placeholder.com/50",
+    muted: false,
+  },
+];
