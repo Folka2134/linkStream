b33f72afcf865c1c99b6d7c7ec22c0d5c1ff20d9
folka2134 <folka2134@gmail.com>
2024-08-27 11:51:33 +0100

    implemented group provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import ActiveWindow from "./components/ActiveWindow";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import { GroupProvider } from "./context/GroupContext";
 
 function App() {
   return (
-    <>
-      <h1 className="">Test</h1>
-    </>
+    <GroupProvider>
+      <div className="">
+        <Header />
+        <div className="flex h-screen w-screen">
+          <Nav />
+          <ActiveWindow />
+        </div>
+      </div>
+    </GroupProvider>
   );
 }
 
