fc91279fa2e6b17cc2caf6288d69488013922287
folka2134 <folka2134@gmail.com>
2024-08-28 10:46:55 +0100

    updated mock data

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -1,6 +1,13 @@
+export const currentUser = {
+  id: 10,
+  name: "Folka",
+  image: "https://via.placeholder.com/30",
+  status: "online",
+};
+
 export const groups = [
   {
-    id: 4,
+    id: 5,
     name: "Direct Messages",
     image: "https://via.placeholder.com/50",
     muted: false,
@@ -28,5 +35,150 @@ export const groups = [
     name: "Development",
     image: "https://via.placeholder.com/50",
     muted: false,
+  },
+];
+
+export const users = [
+  {
+    id: 1,
+    userName: "John Doe",
+    image: "https://via.placeholder.com/30",
+    status: "online",
+  },
+  {
+    id: 2,
+    userName: "Jane Doe",
+    image: "https://via.placeholder.com/30",
+    status: "offline",
+  },
+  {
+    id: 3,
+    userName: "Jack Smith",
+    image: "https://via.placeholder.com/30",
+    status: "online",
+  },
+  {
+    id: 4,
+    userName: "Tricks",
+    image: "https://via.placeholder.com/30",
+    status: "offline",
+  },
+];
+
+export const chats = [
+  {
+    id: 1,
+    chatId: 123,
+    senderId: 1,
+    recipientId: 10,
+    senderName: "John Doe",
+    senderImage: "https://via.placeholder.com/30",
+    lastMessage: "That's great to hear!",
+    timestamp: "12:02",
+  },
+  {
+    id: 2,
+    chatId: 523,
+    senderId: 2,
+    recipientId: 10,
+    senderName: "Jane Doe",
+    senderImage: "https://via.placeholder.com/30",
+    lastMessage: "That's good to hear.",
+    timestamp: "12:05",
+  },
+  {
+    id: 3,
+    chatId: 623,
+    senderId: 3,
+    recipientId: 10,
+    senderName: "Jack Smith",
+    senderImage: "https://via.placeholder.com/30",
+    lastMessage: "I have to go now, talk to you later.",
+    timestamp: "12:06",
+  },
+  {
+    id: 4,
+    chatId: 711,
+    senderId: 4,
+    recipientId: 10,
+    sendername: "Tricks",
+    senderImage: "https://via.placeholder.com/30",
+    lastMessage: "Yo!",
+    timestamp: "12:08",
+  },
+];
+
+export const messages = [
+  {
+    id: 1,
+    chatId: 123,
+    senderId: 1,
+    recipientId: 10,
+    content: "Hello, how are you?",
+    timestamp: "12:00",
+  },
+  {
+    id: 2,
+    chatId: 523,
+    senderId: 2,
+    recipientId: 10,
+    content: "I'm good, thank you.",
+    timestamp: "12:01",
+  },
+  {
+    id: 3,
+    chatId: 123,
+    senderId: 1,
+    recipientId: 10,
+    content: "That's great to hear!",
+    timestamp: "12:02",
+  },
+  {
+    id: 4,
+    chatId: 523,
+    senderId: 2,
+    recipientId: 10,
+    content: "How about you?",
+    timestamp: "12:03",
+  },
+  {
+    id: 5,
+    chatId: 123,
+    senderId: 1,
+    recipientId: 10,
+    content: "I'm doing well, thanks for asking.",
+    timestamp: "12:04",
+  },
+  {
+    id: 6,
+    chatId: 523,
+    senderId: "2",
+    recipientId: 10,
+    content: "That's good to hear.",
+    timestamp: "12:05",
+  },
+  {
+    id: 7,
+    chatId: 623,
+    senderId: "3",
+    recipientId: 10,
+    content: "I have to go now, talk to you later.",
+    timestamp: "12:06",
+  },
+  {
+    id: 8,
+    chatId: 523,
+    senderId: "2",
+    recipientId: 10,
+    content: "Bye!",
+    timestamp: "12:07",
+  },
+  {
+    id: 9,
+    chatId: 711,
+    senderId: "4",
+    recipientId: 10,
+    content: "Yo!",
+    timestamp: "12:08",
   },
 ];
