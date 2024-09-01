52c02b45ff01f889dedd0b72e15c58ed91717776
folka2134 <folka2134@gmail.com>
2024-09-01 15:48:04 +0100

    updated types

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,8 +1,8 @@
 import { ReactNode, createContext, useState } from "react";
 
 export interface ChatContextInterface {
-  selectedChatId: Number | null;
-  setSelectedChatId: React.Dispatch<React.SetStateAction<Number | null>>;
+  selectedChatId: String | null;
+  setSelectedChatId: React.Dispatch<React.SetStateAction<String | null>>;
 }
 
 export const ChatContext = createContext<ChatContextInterface | undefined>(
@@ -10,7 +10,7 @@ export const ChatContext = createContext<ChatContextInterface | undefined>(
 );
 
 export const ChatProvider = ({ children }: { children: ReactNode }) => {
-  const [selectedChatId, setSelectedChatId] = useState<Number | null>(null);
+  const [selectedChatId, setSelectedChatId] = useState<String | null>(null);
 
   return (
     <ChatContext.Provider value={{ selectedChatId, setSelectedChatId }}>
