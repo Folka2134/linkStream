0a0e50aa3f5f99c06efc33d99b54af2711248a8a
folka2134 <folka2134@gmail.com>
2024-09-01 16:16:30 +0100

    displaying selectedUser details

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -2,7 +2,7 @@ import { useMessage } from "@/hooks/useMessage";
 import { useUser } from "@/hooks/useUser";
 
 const ChatArea = ({ selectedChatId }: any) => {
-  const { userMap } = useUser();
+  const { userMap, selectedUserId } = useUser();
   const { messageMap } = useMessage();
 
   // loop over messages and appending to messages array
@@ -13,31 +13,35 @@ const ChatArea = ({ selectedChatId }: any) => {
     }
   }
 
-  console.log(messages);
+  let selectedUser = userMap[selectedUserId!];
 
   return (
-    <div>
-      <h4>Chat Area</h4>
+    <div className="w-full">
+      <div className="flex h-12 items-center bg-primary">
+        <h4>{selectedUser.userName}</h4>
+      </div>
 
-      {/* // loop over messages and display content in order of timestamp */}
-      {messages
-        .sort((a, b) => a.timestamp - b.timestamp)
-        .map((message) => {
-          const user = userMap[message.userId];
-          return (
-            <div key={message.id} className="flex gap-2">
-              <img
-                src={user.userImage}
-                alt={user.userName}
-                className="h-8 w-8 rounded-full"
-              />
-              <div>
-                <p>{user.userName}</p>
-                <p>{message.content}</p>
+      <div className="p-3">
+        {/* // loop over messages and display content in order of timestamp */}
+        {messages
+          .sort((a, b) => a.timestamp - b.timestamp)
+          .map((message) => {
+            const user = userMap[message.userId];
+            return (
+              <div key={message.id} className="flex gap-2">
+                <img
+                  src={user.userImage}
+                  alt={user.userName}
+                  className="h-8 w-8 rounded-full"
+                />
+                <div>
+                  <p>{user.userName}</p>
+                  <p>{message.content}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </div>
   );
 };
