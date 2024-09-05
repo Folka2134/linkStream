78b9a90a4fc8206a990d7a59a07ceb9c75bea7de
folka2134 <folka2134@gmail.com>
2024-09-05 12:47:36 +0100

    changed name to group

---
+++ src/components/Groups.tsx
@@ -0,0 +1,33 @@
+import { groups } from "@/lib/mockData";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { useGroup } from "@/hooks/useGroup";
+
+const Groups = () => {
+  const { setSelectedGroup } = useGroup();
+
+  return (
+    <div className="flex w-24 justify-center">
+      <ul className="mt-5 flex flex-col gap-2">
+        {groups.map((group) => (
+          <TooltipProvider key={group.id}>
+            <Tooltip>
+              <TooltipTrigger onClick={() => setSelectedGroup(group.name)}>
+                <img src={group.image} alt={group.name} className="h-10 w-10" />
+              </TooltipTrigger>
+              <TooltipContent className="bg-secondary">
+                <p>{group.name}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Groups;
