7d98655d84dcc4049b977199a37bd2f3ac91c7d1
folka2134 <folka2134@gmail.com>
2024-09-09 10:46:55 +0100

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,13 @@ const Header = () => {
 
   return (
     <div className="flex justify-between p-2">
-      <h1 className="logo">LinkStream</h1>
+      <h1 className="logo">
+        {/* <img
+          src="/assets/images/link_stream_transparent.png"
+          className="h-20 w-20"
+        /> */}
+        Link Stream
+      </h1>
       <Switch className="bg-accent" onCheckedChange={handleThemeChange} />
     </div>
   );
